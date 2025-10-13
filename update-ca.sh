f9032b2a1dce6eaaab46eba4f6b967408c089ff3
Ez <ezy.misc@gmail.com>
2025-10-13 19:49:59 +0530

    corrected the update-ca script

--- update-ca.sh
+++ update-ca.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo "Updating CA certs..."
-curl -s -o ~/AdGuardHome/cacert.pem https://curl.se/ca/cacert.pem
-chmod 644 ~/AdGuardHome/cacert.pem
+curl -s -o $PREFIX/etc/tls/cacert.pem https://curl.se/ca/cacert.pem
+chmod 644 $PREFIX/etc/tls/cacert.pem
 echo "CA bundle updated on $(date)" >> ~/ca-update.log
 
