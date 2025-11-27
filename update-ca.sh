5a77a0c0d1787ef048b313103d32a482bcef494d
Ez <128786297+cosmos-creator@users.noreply.github.com>
2025-11-27 15:18:20 +0000

    Delete update-ca.sh
    junk

--- update-ca.sh
+++
@@ -1,6 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-echo "Updating CA certs..."
-curl -s -o $PREFIX/etc/tls/cacert.pem https://curl.se/ca/cacert.pem
-chmod 644 $PREFIX/etc/tls/cacert.pem
-echo "CA bundle updated on $(date)" >> ~/ca-update.log
-
